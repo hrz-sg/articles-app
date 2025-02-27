fa92a3736edebe069950f37a984c8e6f3584f1ae
hrz-sg <im.shoo@yandex.ru>
2025-02-27 17:00:47 +0800

    add scss webpack, React

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+import Counter from './components/Counter';
+
+const root = createRoot(
+    document.getElementById('root')
+)
+
+root.render(
+    <div>
+        <Counter />
+    </div>,
+)
