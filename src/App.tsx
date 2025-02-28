24d91e31558682dd2666e52ca48540015ea7e901
hrz-sg <im.shoo@yandex.ru>
2025-02-28 20:17:33 +0800

    add code splitting, lazy suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import Counter from './components/Counter';
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Link } from 'react-router-dom';
+import { Suspense } from 'react';
 
 const App = () => {
   return (
     <div className="app">
-        AKAKKAKAKKAK
-      <Counter />
+      <Link to={'/'}>MAIN</Link>
+      <Link to={'/about'}>ABOUT</Link>
+        <Suspense fallback={<div>Loading ...</div>}>
+          <Routes>
+              <Route path={'/about'} element={<AboutPageAsync />} />
+              <Route path={'/'} element={<MainPageAsync />}/>
+          </Routes>
+        </Suspense>
     </div>
   )
 }
