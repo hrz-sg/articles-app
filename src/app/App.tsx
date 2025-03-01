e03757d4db1270ebbf83777ed64848dcd4f730a7
hrz-sg <im.shoo@yandex.ru>
2025-03-02 02:19:38 +0800

    added fsd, fixed import paths

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Link } from 'react-router-dom';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider/lib/useTheme';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
@@ -17,8 +17,8 @@ const App = () => {
       <Link to={'/about'}>ABOUT</Link>
         <Suspense fallback={<div>Loading ...</div>}>
           <Routes>
-              <Route path={'/about'} element={<AboutPageAsync />} />
-              <Route path={'/'} element={<MainPageAsync />}/>
+              <Route path={'/about'} element={<AboutPage />} />
+              <Route path={'/'} element={<MainPage />}/>
           </Routes>
         </Suspense>
     </div>
