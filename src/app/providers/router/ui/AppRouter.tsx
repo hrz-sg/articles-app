f9a8532b99e1dbcaada0253ccbfa2407a40a948c
hrz-sg <im.shoo@yandex.ru>
2025-03-12 22:22:03 +0800

    add Sidebar, layout

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -4,18 +4,24 @@ import { routeConfig } from "shared/config/routeConfig/routeConfig"
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading ...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({element, path}) => (
-              <Route 
-                key={path}
-                path={path}
-                element={element}
-              />
-            ))}
-        </Routes>
-    </Suspense>
-  )
-}
+      <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+              {Object.values(routeConfig).map(({element, path}) => (
+                  <Route
+                      key={path}
+                      path={path}
+                      element={(
+                          <Suspense fallback={<div>Loading...</div>}>
+                              <div className="page-wrapper">
+                                  {element}
+                              </div>
+                          </Suspense>
+                      )}
+                  />
+              ))}
+          </Routes>
+      </Suspense>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
